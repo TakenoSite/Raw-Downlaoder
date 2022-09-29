d9ad03b16566d7ce7fd7f8f35cdce8caebd797a8
TakenServer <84880497+TakenoSite@users.noreply.github.com>
2022-09-29 18:47:37 +0900

    add Compile Flas

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ set -e
 echo "[*]Install.."
 pip3 install flask
 echo "[*]Compiling.."
-gcc ./src/module/main.c ./src/module/downloadModule.c -o SimpleDownloader
+gcc -Wall -O2 ./src/module/main.c ./src/module/downloadModule.c -o SimpleDownloader
 echo "[*]Set Server"
 echo "python3 ./src/server/server.py" > server_run.sh
 chmod +x server_run.sh
